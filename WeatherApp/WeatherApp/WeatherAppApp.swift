e3fc8227f872c89b4338de859d558f8bfd02138e
mollysandler <molly.sandler13@gmail.com>
2024-10-10 10:35:20 -0700

    swift initial files

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Molly Sandler on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
